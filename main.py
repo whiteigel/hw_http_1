f2f289e4279e1703cd8f3fcc00de9f58eeeba251
Sergey Volodin <whiteigel@gmail.com>
2021-07-08 21:28:56 +0200

    Done without loops

--- main.py
+++ main.py
@@ -11,19 +11,15 @@ class Superhero:
         if response.status_code != 200:
             print("ERROR")
             return
-        res = response.json()['results']
-        for ind, elm in enumerate(res):
-            name = elm['name']
-            # id = elm['id']
-            intelligence = elm['powerstats']['intelligence']
-            stats = {}
-            stats[name] = intelligence
-            return stats
+        intel = response.json()['results'][0]['powerstats']['intelligence']
+        name = response.json()['results'][0]['name']
+        stats = {}
+        stats[name] = int(intel)
+        return stats
 
     def compare_heros(self, heroes):
         dict_hero = {}
         for ind, hero in enumerate(heroes):
-            # print(hero)
             for s_hero, stat in hero.items():
                 dict_hero[s_hero] = int(stat)
         super_intel_hero = max(dict_hero, key=dict_hero.get)
@@ -35,5 +31,6 @@ if __name__ == '__main__':
     captain = Superhero("Captain America")
     heroes_list = [thanos.search_name(), hulk.search_name(), captain.search_name()]
     print(hulk.compare_heros(heroes_list))
+
 
 
