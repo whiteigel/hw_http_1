fb0591bebf8aee76b04ac7a3a8cfcd421d4e2819
Sergey Volodin <whiteigel@gmail.com>
2021-07-08 02:50:32 +0200

    Class added. Logic changed according to class

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 import requests
 
-def search_name(superhero):
-    name = superhero
-    url = f'https://superheroapi.com/api/2619421814940190/search/{name}'
-    response = requests.get(url=url)
-    if response.status_code != 200:
-        print("ERROR")
-        return
-    res = response.json()['results']
-    for ind, elm in enumerate(res):
-        name = elm['name']
-        # id = elm['id']
-        intelligence = elm['powerstats']['intelligence']
-        stats = {}
-        stats[name] = intelligence
-        return stats
+class Superhero:
+    def __init__(self, name):
+        self.name = name
 
-def compare_heros(heroes):
-    dict_hero = {}
-    for ind, hero in enumerate(heroes):
-        # print(hero)
-        for s_hero, stat in hero.items():
-            dict_hero[s_hero] = int(stat)
-    super_intel_hero = max(dict_hero, key=dict_hero.get)
-    return f'Самый умный из супер-героев: {super_intel_hero}'
+    def search_name(self):
+        name = self.name
+        url = f'https://superheroapi.com/api/2619421814940190/search/{name}'
+        response = requests.get(url=url)
+        if response.status_code != 200:
+            print("ERROR")
+            return
+        res = response.json()['results']
+        for ind, elm in enumerate(res):
+            name = elm['name']
+            # id = elm['id']
+            intelligence = elm['powerstats']['intelligence']
+            stats = {}
+            stats[name] = intelligence
+            return stats
+
+    def compare_heros(self, heroes):
+        dict_hero = {}
+        for ind, hero in enumerate(heroes):
+            # print(hero)
+            for s_hero, stat in hero.items():
+                dict_hero[s_hero] = int(stat)
+        super_intel_hero = max(dict_hero, key=dict_hero.get)
+        return f'Самый умный из супер-героев: {super_intel_hero}'
 
 if __name__ == '__main__':
-    heroes_list = [search_name('Hulk'), search_name('Thanos'), search_name('Captain America')]
-    print(compare_heros(heroes_list))
+    thanos = Superhero("Thanos")
+    hulk = Superhero("Hulk")
+    captain = Superhero("Captain America")
+    heroes_list = [thanos.search_name(), hulk.search_name(), captain.search_name()]
+    print(hulk.compare_heros(heroes_list))
+
 
